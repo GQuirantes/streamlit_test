523b6414bf383f3575c44ab01e0ab8de3ebf7cea
quirantes <quirantes.mendes@gmail.com>
2025-03-03 17:25:43 -0300

    authentication

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st 
+from src.authentication import show_authenticator
+
+show_authenticator()
+
 st.title('o Pau vai tora!')
 
 st.write("vamos testar")
---
+++ src/authentication.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import asyncio
+
+# Gerenciar o estado da sessão
+ss = st.session_state
+
+def authenticate(username: str, password: str) -> bool:
+    """Verifica se o usuário e a senha estão corretos com base nos segredos armazenados."""
+    logins = st.secrets
+
+    for _, user_data in logins.login.items():
+        if username == user_data['USERNAME'] and password == user_data['PASSWORD']:
+            ss.username = user_data['USERNAME']
+            ss.user_role = user_data['ROLE']
+            ss.authenticated = True  # Define o estado autenticado como True
+            return True
+
+    return False
+
+def show_authenticator():
+    """Exibe o formulário de login e autentica o usuário."""
+    # Inicializa o estado da sessão para autenticação
+    if 'authenticated' not in ss:
+        ss.authenticated = False
+    if 'username' not in ss:
+        ss.username = None
+    if 'password' not in ss:
+        ss.password = None
+
+    # Se o usuário ainda não está autenticado, exibe o formulário de login
+    if not ss.authenticated:
+        with st.columns([3])[1]:  # Centraliza o formulário
+            st.title('Login')
+            username = st.text_input('Username')
+            password = st.text_input('Password', type='password')
+
+            if st.button('Login'):
+                if authenticate(username, password):
+                    st.success('Login realizado com sucesso!')
+                    asyncio.sleep(1)  # Aguarda 1 segundo antes de recarregar a página
+                    st.rerun()  # Recarrega a aplicação para atualizar o estado
+                else:
+                    st.error('O username ou password está incorreto')
+
+    st.stop()  # Impede que o código continue sendo executado caso o usuário não esteja autenticado
+
